13d92dd934e732f9748ba41dc440bf1e8d4cfd3e
刘述清 <nancheng@outlook.com>
2015-09-24 18:31:52 +0800

    加了一点东西

--- src/Test01.java
+++ src/Test01.java
@@ -1,9 +1,21 @@
 public class Test01 {
     public static int a=3;
     public static void main(String args[]) {
-        int[][] array = { { 1, 3, 4 }, { 4, 5, 6 }, { 6, 7, 8 }, { 11, 12, 13 }, { 12, 14, 15 } };
+        mTest test=new mTest();
+        test.name="a";
+        System.out.println(test.name);
+        testPbjectSend(test);
+        System.out.println(test.name);
+       /* int[][] array = { { 1, 3, 4 }, { 4, 5, 6 }, { 6, 7, 8 }, { 11, 12, 13 }, { 12, 14, 15 } };
         System.out.println(isFind(array, 15));
-        testIn();
+        testIn();*/
+    }
+    
+    public static void test(int a, String ... books) {
+        for (String tem : books) {
+            System.out.println(tem);
+        }
+        System.out.println(a);
     }
 
     public static void testIn(){
@@ -31,6 +43,11 @@ public class Test01 {
         return false;
     }
 
+    /**
+     * 替换空格
+     * @param str
+     * @return
+     */
     public String replaceSpace(StringBuffer str) {
         StringBuffer bf = new StringBuffer();
         char[] chars = str.toString().toCharArray();
@@ -42,5 +59,17 @@ public class Test01 {
             }
         }
         return bf.toString();
+    }
+    
+    /**
+     * 卧槽,发现了惊天大秘密,原来java里面存在引用传递,也就是说,一个对象传递给
+     * 方法,方法的操作会直接影响这个对象的堆
+     * @param m
+     */
+    public static void testPbjectSend(mTest m){
+        m.name="bbb";
+    }
+    private static class mTest{
+        String name;
     }
 }
